8343bd8495091d73e26032d6cbf73e67cf507b8c
Malik8008 <tu60prbs1@code.edu.az>
2022-04-04 03:39:53 +0400

    DoubleArr-completed

---
+++ Delegate-DoubleArr-Task/Delegate-DoubleArr-Task/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Delegate_DoubleArr_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] Product = new double[] { 15.2, 25, 34.6, 54, 67.99, 78 };
+
+            Action<double[]> actcount = GetCount;            
+            Action<double[]> acttotal = GetTotal;
+
+
+
+            actcount += acttotal;
+            actcount(Product);
+
+        }
+
+        public static void GetCount(double[] arr)
+        {
+            foreach (var num in arr.Length.ToString())
+            {
+                Console.WriteLine($"ProductCount: {num}");
+            }
+        }
+
+        public static void GetTotal(double[] arr)
+        {
+            double total=0;
+            foreach (var item in arr)
+            {
+                total += item;
+                
+            }
+            Console.WriteLine($"ProductTotal: {total}");
+        }
+        
+    }
+}
